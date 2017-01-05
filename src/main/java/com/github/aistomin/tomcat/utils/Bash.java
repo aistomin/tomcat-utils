51acb0d1b6ffeaf9e9324a142226f57559a52b15
aistomin <andrej.istomin.ikeen@gmail.com>
2017-01-05 11:16:13 +0100

    Issue #24. Bash.java interface was added.

---
+++ src/main/java/com/github/aistomin/tomcat/utils/Bash.java
@@ -0,0 +1,40 @@
+/**
+ * Copyright (c) 2016, Istomin Andrei
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.github.aistomin.tomcat.utils;
+
+/**
+ * Bash scripts executor.
+ *
+ * @author Andrei Istomin (andrej.istomin.ikeen@gmail.com)
+ * @version $Id$
+ * @since 0.1
+ */
+public interface Bash {
+
+    /**
+     * Execute bash command synchronously.
+     * @param cmd Bash command that needs to be executed.
+     * @return Exit code.
+     */
+    int executeSync(String cmd);
+
+    /**
+     * Execute bash command asynchronously.
+     * @param cmd Bash command that needs to be executed.
+     * @return Exit code.
+     */
+    int executeAsync(String cmd);
+}
